b959e849601882e30deedbdb86661f1c5a1f853a
Nailuj29 Gaming <nailuj29@nailuj29gaming@gmail.com>
2021-06-07 19:57:18 -0400

    BEE

--- src/bitcoin-tx.cpp
+++ src/bitcoin-tx.cpp
@@ -97,9 +97,9 @@ static int AppInitRawTx(int argc, char* argv[])
 
     if (argc < 2 || HelpRequested(gArgs)) {
         // First part of help message is specific to this utility
-        std::string strUsage = PACKAGE_NAME " litecoin-tx utility version " + FormatFullVersion() + "\n\n" +
-            "Usage:  litecoin-tx [options] <hex-tx> [commands]  Update hex-encoded litecoin transaction\n" +
-            "or:     litecoin-tx [options] -create [commands]   Create hex-encoded litecoin transaction\n" +
+        std::string strUsage = PACKAGE_NAME " apiocoin-tx utility version " + FormatFullVersion() + "\n\n" +
+            "Usage:  apiocoin-tx [options] <hex-tx> [commands]  Update hex-encoded apiocoin transaction\n" +
+            "or:     apiocoin-tx [options] -create [commands]   Create hex-encoded apiocoin transaction\n" +
             "\n";
         strUsage += gArgs.GetHelpMessage();
 
